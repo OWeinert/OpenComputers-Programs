c20bc2044ab209f36e82bdb7871d34ad59ca9530
Piggo41 <mr.piggo123@gmail.com>
2024-04-20 07:01:22 +0200

    debug build

--- reactor-control/reactorctl.lua
+++ reactor-control/reactorctl.lua
@@ -99,7 +99,7 @@ local function reactorCoroutine()
             gpu.set(1, row, "██")
 
             -- Draw fuel name
-            if not reactorStats.fuelName == "No Fuel" then
+            if not (reactorStats.fuelName == "No Fuel") then
                 gpu.setForeground(colors.white)
                 gpu.set(4, row, "Fuel: " .. fuelName .. "    ")
             end
