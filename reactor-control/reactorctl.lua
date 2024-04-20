080568ebd7e4d9df160096f4f2e9286bacd4907f
Piggo41 <mr.piggo123@gmail.com>
2024-04-20 15:42:45 +0200

    debug build

--- reactor-control/reactorctl.lua
+++ reactor-control/reactorctl.lua
@@ -1,6 +1,8 @@
 local component = require("component")
 local colors = require("screenColors")
 local text = require("text")
+local thread = require("thread")
+local keyboard = require("keyboard")
 
 if not component.isAvailable("nc_fission_reactor") then
     print("No NuclearCraft FissionReactor detected!")
@@ -16,6 +18,7 @@ local gpu = component.gpu
 
 -- reactors consisting of address and proxy to the nc_fission_reactor
 local reactors = {}
+local lastReactorStats = {}
 local reactorCount = 0
 
 --[[
@@ -40,10 +43,17 @@ end
 
 function initReactors()
     -- Find all connected reactors
-    for address, _ in component.list("nc_fission_reactor") do
-        reactors[address] = component.proxy(address)
+    for addr, _ in component.list("nc_fission_reactor") do
+        reactors[addr] = component.proxy(addr)
         reactorCount = reactorCount + 1
     end
+
+    reactorThread = thread.create(function()
+        for addr, proxy in pairs() do
+            local stats = getReactorStats(proxy)
+            reactorData[addr] = stats
+        end
+    end)
 end
 
 
@@ -84,44 +94,57 @@ local function reactorCoroutine()
         local row = 0
         for addr, proxy in pairs(reactors) do
             local reactorStats = getReactorStats(proxy)
+            if lastReactorStats == nil then
+                lastReactorStats = reactorStats
+            end
 
             -- Draw activity
-            local activityColor = colors.red
-            if reactorStats.isActive then
-                activityColor = colors.green
+            if not (lastReactorStats.isActive == reactorStats.isActive) then
+                local activityColor = colors.red
+                if reactorStats.isActive then
+                    activityColor = colors.green
+                end
+                gpu.setForeground(activityColor)
+                gpu.set(1, row, "██")
             end
-            gpu.setForeground(activityColor)
-            gpu.set(1, row, "██")
 
             -- Draw fuel name
-            gpu.setForeground(colors.white)
-            gpu.set(4, row, "Fuel: " .. reactorStats.fuelName .. "    ")
-
-            -- Draw progressbar
-            local roundedTotalProcessTime = math.floor(reactorStats.totalProcessTime)
-
-            if roundedTotalProcessTime > 0 then
-                local rawProgress = math.floor(reactorStats.currentProcessTime / roundedTotalProcessTime * 10)
-                local clampedProgress = math.min(math.max(rawProgress, 0), 10)
-                gpu.setForeground(colors.red)
-                gpu.set(22, row, string.rep("█", clampedProgress))
-                gpu.setForeground(colors.green)
-                gpu.set(22 + clampedProgress, row, string.rep("█", 10 - clampedProgress))
-            else
-                gpu.setForeground(colors.red)
-                gpu.set(22, row, string.rep("█", 10))
+            if not (lastReactorStats.fuelName == reactorStats.fuelName) then
+                gpu.setForeground(colors.white)
+                gpu.set(4, row, "Fuel: " .. reactorStats.fuelName .. "    ")
             end
 
-            local timeLeft = math.max(math.floor((roundedTotalProcessTime - reactorStats.currentProcessTime) / 20), 0)
-            gpu.setForeground(colors.white)
-            gpu.set(34, row, text.padRight("Time Left: " .. timeLeft .. " s", 18) .. "Power: " .. reactorStats.power)
+            -- Draw progressbar
+            if not (lastReactorStats.totalProcessTime == reactorStats.totalProcessTime)
+                or not (lastReactorStats.currentProcessTime == reactorStats.currentProcessTime) then
 
-            gpu.set(80, row, addr)
+                local roundedTotalProcessTime = math.floor(reactorStats.totalProcessTime)
+
+                if roundedTotalProcessTime > 0 then
+                    local rawProgress = math.floor(reactorStats.currentProcessTime / roundedTotalProcessTime * 10)
+                    local clampedProgress = math.min(math.max(rawProgress, 0), 10)
+                    gpu.setForeground(colors.red)
+                    gpu.set(22, row, string.rep("█", clampedProgress))
+                    gpu.setForeground(colors.green)
+                    gpu.set(22 + clampedProgress, row, string.rep("█", 10 - clampedProgress))
+                else
+                    gpu.setForeground(colors.red)
+                    gpu.set(22, row, string.rep("█", 10))
+                end
+
+                local timeLeft = math.max(math.floor((roundedTotalProcessTime - reactorStats.currentProcessTime) / 20), 0)
+                gpu.setForeground(colors.white)
+                gpu.set(34, row, text.padRight("Time Left: " .. timeLeft .. " s", 18))
+            end
+
+            if not (lastReactorStats.power == reactorStats.power) then
+                gpu.set(52, row, "Power: " .. reactorStats.power)
+            end
+
             row = row + 2
-            os.sleep(0)
+            lastReactorStats[addr] = reactorStats
             coroutine.yield()
         end
-        os.sleep(0)
         coroutine.yield()
     end
 end
@@ -131,17 +154,29 @@ function main()
     initReactors()
     initScreen()
 
+
     drawSeparator(1)
     for i = 0, reactorCount do
         drawSeparator((2 * i) + 1)
     end
 
-    local c = coroutine.create(reactorCoroutine)
-    coroutine.resume(c)
+    local t = thread.create(function()
+        local c = coroutine.create(reactorCoroutine)
+        while true do
+            coroutine.resume(c)
+            os.sleep(0)
+        end
+    end)
+
     while true do
-        coroutine.resume(c)
+        if keyboard.isControlDown() and keyboard.isKeyDown("w") then
+            break;
+        end
         os.sleep(0)
     end
+
+    os.sleep(0)
+    t:kill()
 
     local maxWidth, maxHeight = gpu.maxResolution()
     gpu.setViewport(maxWidth, maxHeight)
