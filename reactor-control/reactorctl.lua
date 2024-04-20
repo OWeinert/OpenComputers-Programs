35b19490c1f95fa4b8abc6696a2492d643442456
Piggo41 <mr.piggo123@gmail.com>
2024-04-20 04:39:02 +0200

    changed coroutine.yield() to os.sleep()

--- reactor-control/reactorctl.lua
+++ reactor-control/reactorctl.lua
@@ -91,7 +91,17 @@ local function drawReactorStats(reactorStats, rowIndex)
     gpu.set(3, rowIndex, "Fuel: " .. reactorStats.fuelName)
 
     -- Draw progressbar
-
+    --[[
+        local progress = math.floor(reactorStats.currentProcessTime / reactorStats.totalProcessTime * 10)
+        gpu.setForeground(colors.green)
+        for i = 0, progress do
+            gpu.set(17 + i, rowIndex, "█")
+        end
+        gpu.setForeground(colors.red)
+        for j = progress, 10 do
+            gpu.set(17 + j, rowIndex, "█")
+        end
+    ]]
     gpu.set(17, reactorStats.currentProcessTime .. "/" .. reactorStats.totalProcessTime)
 
     -- Draw generated power
@@ -108,9 +118,9 @@ local function updateThread()
             drawReactorStats(reactorStats, rowIndex)
             drawSeparator(rowIndex + 1)
             rowIndex = rowIndex + 2
-            coroutine.yield()
+            os.sleep()
         end
-        coroutine.yield()
+        os.sleep()
     end
 end
 
@@ -126,7 +136,7 @@ function main()
             t:kill()
             break
         end
-        os.sleep(0)
+        os.sleep()
     end
 
     local maxWidth, maxHeight = gpu.maxResolution()
